fb56b93840c683da992a0177c9811ee3825beb25
iTzBrassy <77305703+iTzBrassy@users.noreply.github.com>
2022-06-05 13:06:48 -0400

    Add files via upload

---
+++ Exercise3/Exercises.js
@@ -0,0 +1,43 @@
+
+//// exercise 3
+// let firstNumber = prompt("enter first number");
+// let secondNumber = prompt("enter second number");
+// let sum = firstNumber + secondNumber;
+// alert(sum);
+
+//// bonus ////
+
+////subtract
+// let firstNumber = prompt("enter first number");
+// let secondNumber = prompt("enter second number");
+// let answer = firstNumber - secondNumber;
+// alert(answer);
+
+////multiply
+// let firstNumber = prompt("enter first number");
+// let secondNumber = prompt("enter second number");
+// let answer = firstNumber * secondNumber;
+// alert(answer);
+
+////divide
+// let firstNumber = prompt("enter first number");
+// let secondNumber = prompt("enter second number");
+// let answer;
+// if(secondNumber == 0) {
+//     alert("you cannot divide by zero")
+// }
+// else {
+//     answer = firstNumber / secondNumber;
+//     alert(answer);
+// }
+
+//// ecercise 4
+
+// let userAge = prompt("How old are you?");
+// if(userAge < 18){
+//     alert("Sorry, you are too young to drive this vehicle.. Powering Off.");
+// } else if (userAge == 18) {
+//     alert("Congratulations on your first year of driving. Enjoy the ride!");
+// } else {
+//     alert("Powering On. Enjoy the ride!");
+// };
